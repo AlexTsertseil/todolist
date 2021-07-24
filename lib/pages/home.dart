57c7dada190ac2b1fcdde507539271a531cd5562
AlexTsertseil <alex.tsertseil@gmail.com>
2021-07-24 23:11:25 +0300

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late String _userToDO;
+  List todoList = [];
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    todoList
+        .addAll(['9:00 Проснутся', '12:00 Сломать шею гусю', '21:00 Отбой']);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Список дел'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (BuildContext, int index) {
+            return Dismissible(
+              key: Key(todoList[index]),
+              child: Card(
+                child: ListTile(
+                  title: Text(todoList[index]),
+                  trailing: IconButton(
+                    icon: Icon(
+                      Icons.delete_forever,
+                      color: Colors.red,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        todoList.removeAt(index);
+                      });
+                    },
+                  ),
+                ),
+              ),
+              onDismissed: (direction) {
+                setState(() {
+                  todoList.removeAt(index);
+                });
+                //if(direction == DismissDirection.endToStart)
+              },
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text('Добавить элемент'),
+                  content: TextField(
+                    onChanged: (String value) {
+                      _userToDO = value;
+                    },
+                  ),
+                  actions: [
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            todoList.add(_userToDO);
+                          });
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Добавить'))
+                  ],
+                );
+              });
+        },
+        child: Icon(
+          Icons.add_box,
+          color: Colors.amber,
+        ),
+      ),
+    );
+  }
+}
